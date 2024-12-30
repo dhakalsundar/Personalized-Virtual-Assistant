5544258ea054da3ad21bfb7a5f8dc02625b05bbe
dhakalsundar <dhakalsundar440@gmail.com>
2024-12-30 09:53:49 +0545

    login

--- login.js
+++ login.js
@@ -4,10 +4,12 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value;
 
+    const message = document.getElementById('login-message');
     if (email === "test@example.com" && password === "password123") {
-        document.getElementById('login-message').textContent = "Login successful!";
-        document.getElementById('login-message').style.color = "green";
+        message.textContent = "Login successful!";
+        message.style.color = "green";
     } else {
-        document.getElementById('login-message').textContent = "Invalid email or password.";
+        message.textContent = "Invalid email or password.";
+        message.style.color = "red";
     }
 });
